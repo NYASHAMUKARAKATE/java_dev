741053b6af939a06c0d28898d3fce96d40e23695
NYASHAMUKARAKATE <mukarakatenyasha34@gmail.com>
2026-01-03 15:30:47 +0200

    created the account class

---
+++ src/Account.java
@@ -0,0 +1,25 @@
+public abstract class Account {
+    private String accountNumber;
+    private String accountHolder;
+    protected double balance; // this is protected so that the subclasses can access it easily
+
+    public Account(String accountNumber, String accountHolder, double balance){
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+
+    }
+    public void deposit(double amount){
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: $" + amount);
+        }
+    }
+    public abstract boolean withdraw(double amount);
+
+    //Getters
+    public String getAccountNumber() { return accountNumber; }
+    public String getAccountHolder() { return accountHolder; }
+    public double getBalance() { return balance; }
+}
+
