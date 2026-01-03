fb9ccabd2d5aeaaff1591c0a6e4c2f3bbbb7250a
NYASHAMUKARAKATE <mukarakatenyasha34@gmail.com>
2026-01-03 16:21:51 +0200

    add methods in the savings account

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -1,3 +1,27 @@
-public class SavingsAccount {
+public class SavingsAccount extends Account {
+    private double interestRate;
 
+    public SavingsAccount(String accountNumber, String accountHolder, double initialBalance, double interestRate) {
+        super(accountNumber, accountHolder, initialBalance);
+        this.interestRate = interestRate;
+    }
+
+    public void applyInterest() {
+        double interest = balance * (interestRate / 100);
+        deposit(interest);
+        System.out.println("Interest applied: $" + interest);
+    }
+
+
+    @Override
+    public boolean withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn from Savings: $" + amount);
+            return true;
+        } else {
+            System.out.println("Insufficient funds in Savings!");
+            return false;
+        }
+    }
 }
